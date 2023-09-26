1d532c3b08370cefea39d45013dfa1c489868687
Alexander Konovalov <alexander.konovalov@coins.ph>
2023-09-26 18:25:49 +0200

    basic functionality works

--- src/main/scala/org/coins/sql/format/SQLFormatterPlugin.scala
+++ src/main/scala/org/coins/sql/format/SQLFormatterPlugin.scala
@@ -17,11 +17,34 @@ object SQLFormatterPlugin extends AutoPlugin {
     formatSQL := {
       val log = streams.value.log
       val baseDir = baseDirectory.value
-
-      // Your SQL formatting logic goes here
-      // For now, we'll just log a message
-      log.info("Formatting SQL embedded in Scala strings...")
-      // You can use libraries like scalafmt to help with the formatting
+      formatScalaFiles(baseDir, log)
     }
   )
+
+  def formatScalaFiles(baseDir: File, log: Logger): Unit = {
+    val scalaFiles = baseDir ** "*.scala"
+
+    scalaFiles.get.foreach { file =>
+      val content = IO.read(file)
+      val formattedContent = formatSQLInString(content)
+      IO.write(file, formattedContent)
+      log.info(s"Formatted SQL in $file")
+    }
+  }
+
+  def formatSQLInString(content: String): String = {
+    val sqlPattern = """spark\.sql\(\n?\s*s?\"\"\"(?s)(.*?)\"\"\"\.stripMargin\)""".r
+    sqlPattern.replaceAllIn(content, m => {
+      val sql = m.group(1)
+      val formattedSQL = formatSQLString(sql)
+      s"""spark.sql(s\"\"\"$formattedSQL\"\"\".stripMargin)"""
+    })
+  }
+
+  def formatSQLString(sql: String): String = {
+    val keywords = Set("SELECT", "FROM", "WHERE", "LEFT JOIN", "INNER JOIN", "WITH", "GROUP BY", "ORDER BY", "AND", ",")
+    keywords.foldLeft(sql) { (acc, keyword) =>
+      acc.replace(keyword, s"\n$keyword")
+    }.split("\n").map(line => line.trim).mkString("\n    ") // 4-space indentation
+  }
 }
